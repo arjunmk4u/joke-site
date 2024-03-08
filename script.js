d337fbbe043ebb07b62ad91b6d642715b0295383
arjunmk4u <arjunmk4u@gmail.com>
2024-03-08 19:20:54 +0530

    Put api call on a try catch block

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ const apiurl = `https://v2.jokeapi.dev/joke/any?type=single`
 const btn = document.getElementById('generatebtn')
 
 async function getjoke(){
-    const response = await fetch(apiurl).then(
-        data => data.json()
-    ).then(
-        jokes => {
-            // console.log(jokes.joke);
-            document.querySelector(".joke").innerHTML = jokes.joke;
-        }
-    )
+    try{
+
+        const response = await fetch(apiurl).then(
+            data => data.json()
+        ).then(
+            jokes => {
+                // console.log(jokes.joke);
+                document.querySelector(".joke").innerHTML = jokes.joke;
+            }
+        )
+    }catch(err){
+        console.log(err.message);
+    }
 };
 
 btn.addEventListener('click', () =>{
     getjoke();
     document.querySelector(".imoji").innerHTML = "😂";
 })
+
+console.clear();
